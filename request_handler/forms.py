f3ec783ca525291e9e09ef42ebd48d3882b7d34d
Enzo Bestetti <enzobestetti3@gmail.com>
2024-11-06 14:41:31 +0000

    Add documentation to forms.py and models.py

--- request_handler/forms.py
+++ request_handler/forms.py
@@ -1,7 +1,11 @@
 import django.forms as forms
 from .models import Request, Day, Modality
 
+""" Class representing a form to create a Request instance.
 
+This class is used as the form displayed to au student user when they wish to make a request for tutoring. They can fill
+in all the fields and a Request instance containing that information will be created and stored in the database.
+"""
 class RequestForm(forms.ModelForm):
 
     available_days = forms.ModelMultipleChoiceField(
--- request_handler/models.py
+++ request_handler/models.py
@@ -1,20 +1,33 @@
-from zoneinfo import available_timezones
-
 from django.core.exceptions import ValidationError
 from django.db import models
 from tutorials.models import User
-# Create your models here.
 
+""" Class representing a day of the week
+
+This Model is necessary for the ManyToMany relationships in Request to work, as they must be between model instances.
+Days are represented in the database as a string (their name) and an automatically assigned id (primary key).
+"""
 class Day(models.Model):
     day = models.CharField(max_length=10)
     def __str__(self):
         return self.day
 
+""" Class representing a mode of attendance
+
+This Model is necessary for the ManyToMany relationships in Request to work, as they must be between model instances.
+Modes are represented in the database as a string (In person, Online, No preference) and an automatically assigned id (primary key).
+"""
 class Modality(models.Model):
     mode = models.CharField(max_length=10)
     def __str__(self):
         return self.mode
 
+""" Class representing a request for lessons from a student.
+
+This Model represents a request made by a student for lessons. It contains all the necessary details for an admin to 
+contact the student and formalise arrangements. These details include: the student's user information, which knowledge areas
+they would like tutoring for, which days of the week they are available, their preferred mode of attendance, etc.
+"""
 class Request(models.Model):
     student = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     knowledge_area = models.CharField(max_length=255,blank=False) #Allow one for now, extend later.
