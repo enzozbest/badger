884d1ec1107d686153e41e56478fc675066b2c85
Lucia Garces <lucia.garces_gutierrez@kcl.ac.uk>
2024-11-08 17:05:07 +0000

    Update field name to match model in test_form.py

--- request_handler/tests_request_handler/forms/test_form.py
+++ request_handler/tests_request_handler/forms/test_form.py
@@ -12,61 +12,61 @@ class TestRequestForm(TestCase):
 
     def test_form_contains_required_fields(self):
         form = RequestForm()
-        self.assertIn('available_days', form.fields)
+        self.assertIn('availability', form.fields)
         self.assertIn('term', form.fields)
         self.assertIn('knowledge_area', form.fields)
         self.assertIn('frequency', form.fields)
         self.assertIn('duration', form.fields)
-        self.assertIn('mode_preference', form.fields)
-        self.assertTrue(isinstance(form.fields['available_days'], ModelMultipleChoiceField))
-        self.assertTrue(isinstance(form.fields['mode_preference'], ModelMultipleChoiceField))
+        self.assertIn('venue_preference', form.fields)
+        self.assertTrue(isinstance(form.fields['availability'], ModelMultipleChoiceField))
+        self.assertTrue(isinstance(form.fields['venue_preference'], ModelMultipleChoiceField))
 
     def test_form_accepts_valid_input(self):
         form_input = {
-            'available_days': [self.monday.id, self.wednesday.id],
+            'availability': [self.monday.id, self.wednesday.id],
             'term': 'Easter',
             'knowledge_area': 'Scala',
             'frequency': 'Weekly',
             'duration': '1h',
-            'mode_preference': [self.in_person.id, self.online.id]
+            'venue_preference': [self.in_person.id, self.online.id]
         }
         form = RequestForm(data=form_input)
         self.assertTrue(form.is_valid(), form.errors)
 
-    def test_form_rejects_blank_available_days_field(self):
+    def test_form_rejects_blank_availability_field(self):
         invalid_input = {
-            'available_days': [],
+            'availability': [],
             'term': 'Easter',
             'knowledge_area': 'Scala',
             'frequency': 'Weekly',
             'duration': '1h',
-            'mode_preference': [self.in_person.id, self.online.id]
+            'venue_preference': [self.in_person.id, self.online.id]
         }
         form = RequestForm(data=invalid_input)
         self.assertFalse(form.is_valid())
-        self.assertIn('available_days', form.errors)
+        self.assertIn('availability', form.errors)
 
     def test_form_rejects_blank_mode_preference_field(self):
         invalid_input = {
-            'available_days': [self.monday.id, self.wednesday.id],
+            'availability': [self.monday.id, self.wednesday.id],
             'term': 'Easter',
             'knowledge_area': 'Scala',
             'frequency': 'Weekly',
             'duration': '1h',
-            'mode_preference': []
+            'venue_preference': []
         }
         form = RequestForm(data=invalid_input)
         self.assertFalse(form.is_valid())
-        self.assertIn('mode_preference', form.errors)
+        self.assertIn('venue_preference', form.errors)
 
     def test_form_rejects_blank_term_field(self):
         invalid_input = {
-            'available_days': [self.monday.id, self.wednesday.id],
+            'availability': [self.monday.id, self.wednesday.id],
             'term': '',
             'knowledge_area': 'Scala',
             'frequency': 'Weekly',
             'duration': '1h',
-            'mode_preference': [self.in_person.id, self.online.id]
+            'venue_preference': [self.in_person.id, self.online.id]
         }
         form = RequestForm(data=invalid_input)
         self.assertFalse(form.is_valid())
@@ -74,12 +74,12 @@ class TestRequestForm(TestCase):
 
     def test_form_rejects_blank_knowledge_area_field(self):
         invalid_input = {
-            'available_days': [self.monday.id, self.wednesday.id],
+            'availability': [self.monday.id, self.wednesday.id],
             'term': 'Easter',
             'knowledge_area': '',
             'frequency': 'Weekly',
             'duration': '1h',
-            'mode_preference': [self.in_person.id, self.online.id]
+            'venue_preference': [self.in_person.id, self.online.id]
         }
         form = RequestForm(data=invalid_input)
         self.assertFalse(form.is_valid())
@@ -87,12 +87,12 @@ class TestRequestForm(TestCase):
 
     def test_form_rejects_blank_frequency_field(self):
         invalid_input = {
-            'available_days': [self.monday.id, self.wednesday.id],
+            'availability': [self.monday.id, self.wednesday.id],
             'term': 'Easter',
             'knowledge_area': 'Scala',
             'frequency': '',
             'duration': '1h',
-            'mode_preference': [self.in_person.id, self.online.id]
+            'venue_preference': [self.in_person.id, self.online.id]
         }
         form = RequestForm(data=invalid_input)
         self.assertFalse(form.is_valid())
@@ -100,12 +100,12 @@ class TestRequestForm(TestCase):
 
     def test_form_rejects_blank_duration_field(self):
         invalid_input = {
-            'available_days': [self.monday.id, self.wednesday.id],
+            'availability': [self.monday.id, self.wednesday.id],
             'term': 'Easter',
             'knowledge_area': 'Scala',
             'frequency': 'Weekly',
             'duration': '',
-            'mode_preference': [self.in_person.id, self.online.id]
+            'venue_preference': [self.in_person.id, self.online.id]
         }
         form = RequestForm(data=invalid_input)
         self.assertFalse(form.is_valid())
