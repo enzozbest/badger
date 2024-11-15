501492a654d42b2dd5dfa3fc5a2d66279846d852
Eeshal Malik <eeshal.malik@kcl.ac.uk>
2024-11-15 23:40:01 +0000

    Add search bar for admins to be able to search for a user.

--- admin_functions/views/view_all_users.py
+++ admin_functions/views/view_all_users.py
@@ -4,6 +4,7 @@ from django.shortcuts import redirect, render
 from tutorials.models import User
 from django.core.paginator import Paginator
 from admin_functions.helpers.filters import UserFilter
+from django.db.models import Q
 
 class AllUsersView(View):
     def get(self, request: HttpRequest) -> HttpResponse:
@@ -14,15 +15,30 @@ class AllUsersView(View):
         if user_type != 'Admin':
             return render(request, 'permission_denied.html', status=403)
 
+        query = request.GET.get('q', '').strip()  # Default to empty string if no query provided
         users = User.objects.all()
         filter_obj = UserFilter(request.GET, queryset=users)
         filtered_users = filter_obj.qs
+
+        if query:
+            filtered_users = filtered_users.filter(Q(first_name__istartswith=query) | Q(last_name__istartswith=query))
+
         paginator = Paginator(filtered_users, 20)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
 
+        user_count = filtered_users.count()
+
+        context = {
+            'users': users,
+            'query': query,
+            'page_obj': page_obj,
+            'count': user_count,
+            'filter': filter_obj,
+        }
+
         user_count = User.objects.count()
-        return render(request, 'view_users.html', {'page_obj': page_obj, 'count': user_count, 'filter':filter_obj})
+        return render(request, 'view_users.html', context)
 
 
     def post(self, request: HttpRequest) -> HttpResponse:
