a1df61d38bc14913791ecd2a3a71d07375dce090
Enzo Bestetti <enzobestetti3@gmail.com>
2024-11-20 15:42:29 +0000

    Add tests for request allocation

--- admin_functions/views/allocate_requests.py
+++ admin_functions/views/allocate_requests.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
+from django.shortcuts import reverse
 from request_handler.models import Request, User, Venue
 from admin_functions.forms import AllocationForm
 from user_system.models import KnowledgeArea, Day
@@ -9,6 +10,9 @@ from user_system.models import KnowledgeArea, Day
 class AllocateRequestView(LoginRequiredMixin, View):
     def get(self, request, request_id):
         lesson_request, suitable_tutors, venues = get_tuple(request_id)
+
+        if lesson_request.allocated:
+            return render(request, 'already_allocated_error.html', status=409)
 
         form = AllocationForm(student=lesson_request.student, tutors=suitable_tutors, venues=venues)
 
@@ -19,20 +23,28 @@ class AllocateRequestView(LoginRequiredMixin, View):
 
     def post(self, request, request_id):
         lesson_request, suitable_tutors, venues = get_tuple(request_id)
+        if lesson_request.allocated:
+            return render(request, 'already_allocated_error.html', status=409)
+
         form = AllocationForm(request.POST, student=lesson_request.student, tutors=suitable_tutors, venues=venues)
         if form.is_valid():
             tutor = form.cleaned_data['tutor']
             day = form.cleaned_data['day']
             venue = form.cleaned_data['venue']
-
             allocate(lesson_request, tutor, Venue.objects.get(id=int(venue)), day)
             update_availabilities(lesson_request, day)
-
             return redirect("view_requests")
 
         return render(request, "allocate_request.html", {
             "form": form,
             "lesson_request": lesson_request,})
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return self.handle_no_permission()
+        if not request.user.is_admin:
+            return render(request, 'permission_denied.html', status=401)
+        return super().dispatch(request, *args, **kwargs)
 
 def get_tuple(id: int) -> tuple:
     lesson_request = get_object_or_404(Request, id=id)
