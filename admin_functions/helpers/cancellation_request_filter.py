ca1c92d114a5d02d5632f5318411bc6b92c9c481
Jiale She <k21151079@kcl.ac.uk>
2024-12-09 13:53:39 +0000

    Fixed Date Searching

--- admin_functions/helpers/cancellation_request_filter.py
+++ admin_functions/helpers/cancellation_request_filter.py
@@ -1,8 +1,7 @@
 import django_filters
 from django.db.models import Q
-
+from datetime import datetime
 from calendar_scheduler.models import Booking
-
 
 class CancellationRequestFilter(django_filters.FilterSet):
 
@@ -12,13 +11,30 @@ class CancellationRequestFilter(django_filters.FilterSet):
     )
 
     def filter_search(self, queryset, name, value):
-        return queryset.filter(
-            Q(id__icontains=value) |
-            Q(student__first_name__icontains=value) |
-            Q(tutor__first_name__icontains=value) |
-            Q(date__icontains=value)
-        )
+        search_filter = Q(student__first_name__icontains=value) | \
+                        Q(tutor__first_name__icontains=value)
+        try:
+            search_date = datetime.strptime(value, "%b. %d, %Y").date() 
+            search_filter |= Q(date=search_date)
+        except ValueError:
+            try:
+                search_date = datetime.strptime(value, "%b %d, %Y").date()
+                search_filter |= Q(date=search_date)
+            except ValueError:
+                try:
+                    search_date = datetime.strptime(value, "%b %d").date()
+                    current_year = datetime.now().year
+                    search_date = search_date.replace(year=current_year)
+                    search_filter |= Q(date=search_date)
+                except ValueError:
+                    pass
+
+        try:
+            search_filter |= Q(id=int(value))
+        except ValueError:
+            pass
         
+        return queryset.filter(search_filter)
 
     class Meta:
         model = Booking
