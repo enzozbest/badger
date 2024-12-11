aa8bbf4f91403ad6fef3ceb903d2abcc419e6b5f
Enzo Bestetti <enzobestetti3@gmail.com>
2024-12-11 13:37:49 +0000

    Refactor forms.py and mixins.py

--- admin_functions/helpers/mixins.py
+++ admin_functions/helpers/mixins.py
@@ -8,7 +8,7 @@ class SortingMixin:
     The fields by which a list can be sorted must be set in the class where the Mixin is used.
     By default, sorting uses the primary key of the elements in the list in ascending order.
     """
-    
+
     valid_sort_fields = []
     default_sort_field = 'pk'
 
@@ -17,10 +17,10 @@ class SortingMixin:
         stripped = sort_field.lstrip('-')
 
         # If the field is not a string, we do not need to worry about upper and lowercase characters
-        if not self.is_string_field(stripped, queryset.model):
+        if not self._is_string_field(stripped, queryset.model):
             return queryset.order_by(sort_field)
 
-        ordering = Upper(stripped).asc()
+        ordering = Upper(stripped)
         if sort_field.startswith('-'):
             ordering = ordering.desc()
         return queryset.order_by(ordering)
@@ -29,7 +29,7 @@ class SortingMixin:
         stripped = field.lstrip('-')
         return field if (stripped in self.valid_sort_fields) else self.default_sort_field
 
-    def is_string_field(self, field_name, model):
+    def _is_string_field(self, field_name, model):
         try:
             field = model._meta.get_field(field_name)
             return field.get_internal_type() in ['CharField', 'TextField']
