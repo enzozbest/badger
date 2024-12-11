75815378a277e1fc7bec84e45d3a44c94d4ca18c
Enzo Bestetti <enzobestetti3@gmail.com>
2024-12-11 21:50:07 +0000

    Update production settings

--- code_tutors/settings.py
+++ code_tutors/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&$dln5wpgorppuw&(gintxm573v2ks+zq4o$(4*lapguixf^+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['13.40.108.194', 'badger2024.ddns.net']
+ALLOWED_HOSTS = ['13.40.108.194', 'badger2024.ddns.net', 'localhost']
 
 # Application definition
 
@@ -119,11 +119,12 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'assets/'
 STATICFILES_DIRS = [
+    BASE_DIR / "assets",
     BASE_DIR / "static",
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected/static/assets'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
