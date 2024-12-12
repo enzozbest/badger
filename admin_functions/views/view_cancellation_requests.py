0fe90b8e6201ed13cc8adddbca7b4a7a5cc66f83
Enzo Bestetti <enzobestetti3@gmail.com>
2024-12-12 11:45:22 +0000

    Add pagination to view_cancellation_requests.py

--- admin_functions/views/view_cancellation_requests.py
+++ admin_functions/views/view_cancellation_requests.py
@@ -1,34 +1,35 @@
-from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse, HttpResponseNotAllowed
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from admin_functions.helpers.mixins import SortingMixin
 from calendar_scheduler.models import Booking
 
 
-class ViewCancellationRequests(View):
+class ViewCancellationRequests(LoginRequiredMixin, ListView, SortingMixin):
     """ Class-based view to display all lesson cancellations that a user has requested
     Cancellations are only requested when the lesson is less than two weeks away
     Only Admin users can access this page
     """
-    def get(self, request: HttpRequest) -> HttpResponse:
+    model = Booking
+    context_object_name = 'cancelled'
+    template_name = 'view_cancellation_requests.html'
+    paginate_by = 20
+    valid_sort_fields = ['id', 'student__full_name', 'tutor__full_name']
+
+    def get_queryset(self):
+        """Method that queries a cancelled bookings"""
+        queryset = super().get_queryset().filter(cancellation_requested=True)
+        return queryset
+
+    def dispatch(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect('log_in')
-        if request.user.user_type != 'Admin':
+            return self.handle_no_permission()
+        if not request.user.user_type == 'Admin':
             return render(request, 'permission_denied.html', status=403)
-        
-        sort = request.GET.get('sort', '')
-        
-        requested_lessons = Booking.objects.filter(cancellation_requested=True)
-
-        if sort:
-            try:
-                requested_lessons = requested_lessons.order_by(sort)
-            except Exception as e:
-                # Handle invalid sorting keys (optional logging)
-                print(f"Invalid sort key: {sort}. Error: {e}")
+        return super().dispatch(request, *args, **kwargs)
 
 
-
-        return render(request, 'view_cancellation_requests.html', context={'lessons':requested_lessons})
-    
-    def post(self, request: HttpRequest) -> HttpResponse:
-        return HttpResponseNotAllowed("This URL only accepts GET requests.", status=405, content=b'Not Allowed')
+def post(self, request: HttpRequest) -> HttpResponse:
+    return HttpResponseNotAllowed("This URL only accepts GET requests.", status=405, content=b'Not Allowed')
